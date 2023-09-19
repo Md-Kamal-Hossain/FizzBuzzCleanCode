58e6d44c808796e461d551179ddd0412d3b3c67c
Md Kamal Hossain <kamal.hossain.monju@gmail.com>
2023-09-19 14:08:57 +0200

    Add project files.

---
+++ FizzBuzzCleanCode/FizzBuzz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzCleanCode
+{
+    public class FizzBuzz
+    {
+        public void DecideFizzOrBuzz(int number)
+        {
+            if (number > 0&&number<101)
+            {
+                if(IsThree(number)  )
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(IsFive(number))
+                {
+                     Console.WriteLine("Buzz");
+                }
+                else if(IsThreeAndFive(number)) 
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
+
+        }
+        
+        public bool IsThree(int number) { return number % 3 == 0; }
+        public bool IsFive(int number) { return number%5==0; }
+        public bool IsThreeAndFive(int number) { return number % 3 == 0 && number % 5 == 0; }
+    }
+}
---
+++ FizzBuzzCleanCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FizzBuzzCleanCode // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Enter a number:");
+            //int num = Convert.ToInt32(Console.ReadLine());            
+            for(int num =0;num<101;num++)
+            {
+                FizzBuzz fizzBuzz = new FizzBuzz();
+                fizzBuzz.DecideFizzOrBuzz(num);
+            }
+            
+        }
+    }
+}
